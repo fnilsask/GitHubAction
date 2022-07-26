9e2c8c416f863ea1367b56efb393c9cf975069d5
Fredrik Nilsask <fredrik.nilsask@gmail.com>
2022-07-26 09:51:40 +0200

    Initial Commit

---
+++ GitHubAction/GitHubActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionApp.swift
+//  GitHubAction
+//
+//  Created by Fredrik Nilsask on 2022-07-26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
